5b36d6e2252c75f4996b021833bf28937182cbf9
Josh <sweetpotatomashed@gmail.com>
2022-11-02 15:12:02 -0600

    Removed STerm prompt

--- Bob_UI.py
+++ Bob_UI.py
@@ -83,10 +83,11 @@ class BobWindow(tk.Frame):
         elif V == "focusin":
             return True
 
+
     def __init__(self, win):
         tk.Frame.__init__(self, win)
         self.lbl1=Label(win, text='Please enter the aid year', fg='red', font=("Times New Roman bold", 13))
-        self.lbl2=Label(win, text='Please enter S term', fg='red', font=("Times New Roman bold", 13))
+        self.lbl2=Label(win, text='Please select S term', fg='red', font=("Times New Roman bold", 13))
        
         aid_valid = (self.register(self.validate_aid_year),
                 '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
@@ -94,32 +95,43 @@ class BobWindow(tk.Frame):
                 '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
 
         self.t1=Entry(bd=10, validate="all", validatecommand=aid_valid)
-        self.t2=Entry(bd=10, validate="all", validatecommand=term_valid)
+        
+        #sTerm = tk.IntVar()
+        #options = ["S", "U", "F"]
+        #self.springTerm=Radiobutton(win, text="Spring", font=("Times New Roman bold", 13), variable=sTerm, value=0)
+        #self.summerTerm=Radiobutton(win, text="Summer", font=("Times New Roman bold", 13), variable=sTerm, value=1)
+        #self.fallTerm=Radiobutton(win, text="Fall", font=("Times New Roman bold", 13), variable=sTerm, value=2)
+
+        #self.t2=Entry(bd=10, validate="all", validatecommand=term_valid)
 
         self.aid_warn_label = Label(win, text="", fg='blue', font=("Times New Roman bold", 13))
         self.aid_warn_label.place(x=570, y=340)
 
-        self.term_warn_label = Label(win, text="", fg='blue', font=("Times New Roman bold", 13))
-        self.term_warn_label.place(x=580, y=440)
+        #self.term_warn_label = Label(win, text="", fg='blue', font=("Times New Roman bold", 13))
+        #self.term_warn_label.place(x=580, y=440)
 
         self.lbl1.place(x=200, y=300)
         self.t1.place(x=600, y=300)
 
-        self.lbl2.place(x=200, y=400)
-        self.t2.place(x=600, y=400)
+        #self.lbl2.place(x=200, y=400)
+        #self.springTerm.place(x=600, y=400)
+        #self.summerTerm.place(x=600, y=430)
+        #self.fallTerm.place(x=600, y=460)
+
+        #self.t2.place(x=600, y=400)
        
        
         #Next pop up window
 
         def open_popup():
             global year_valid
-            global term_valid
-            if aid_valid and term_valid:
+            #global term_valid
+            if year_valid:
                 top= Toplevel(rootWindow)
                 top.geometry("750x250")
                 top.title("Run Window")
                 Label(top, text= "Program running, please do not close the window", font=('Helvetica 18 bold')).place(x=100,y= 80)
-                Do_Queries_Functions.run(self.t1.get(), "5")
+                Do_Queries_Functions.run(self.t1.get())
             else:
                 self.bell()
                 prev = self.focus_get()
