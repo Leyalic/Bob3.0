525a7d0b5a43f8cfe1cb9d5813a86fa376c7fed9
Iman Essaghir <essaghir.iman@gmail.com>
2022-12-09 10:14:08 -0700

    file path im

--- Files/Do_Queries_Functions.py
+++ Files/Do_Queries_Functions.py
@@ -34,7 +34,7 @@ from Files import Second_LDR
 from Files import Tsm_Queries
 
 
-test = True
+test = False
 
 
 # The date becomes the current date and is then placed in MM-DD-YY format
@@ -62,9 +62,9 @@ aid_num_regex = ["[0-9]{2,4}\s*$"]
 date_regex = ["(0*[1-9]|1[012])[-/.](0*[1-9]|[12][0-9]|3[01])[-/.](2\d{3}|\d{2})","(0*[1-9]|[12][0-9]|3[01])[-/.](0*[1-9]|1[012])[-/.](2\d{3}|\d{2})"]
 
 # Directories
-#test_UOSFA_directory = Path("C:/Users/iessaghir/Documents/DoQueries/Destination Folders")
+test_UOSFA_directory = Path("C:/Users/iessaghir/Documents/DoQueries/Destination Folders")
 #test_UOSFA_directory = Path("O:/UOSFA Reports")
-test_UOSFA_directory = Path("C:/Users/JHARDY/Documents/DoQueries/Destination Folders")
+#test_UOSFA_directory = Path("C:/Users/JHARDY/Documents/DoQueries/Destination Folders")
 
 UOSFA_directory = Path("O:/UOSFA Reports")
 
